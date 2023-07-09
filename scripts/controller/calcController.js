5877e1f05b254ecf0ec28743b24922becf6f42da
Mikenson <thomasmikenson40@gmail.com>
2023-07-09 00:14:48 -0300

    commit

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -1,23 +1,63 @@
 class CalcController{
     constructor(){
-        this._displayCalc = "0";
-        this._dataAtual;
+        this._locale = 'pt-BR';
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEl = document.querySelector("#data");
+        this._timeEl = document.querySelector("#hora");
+        this._currentDate;
+        this.initialize();
+    }
+
+    initialize(){
+
+        //this.displayCalcEl.innerHTML = "12345";
+        //this._dateEl.innerHTML = "08/07/2023";
+        //this._timeEl.innerHTML = "22:26";
+            this.setdisplayDateTime();
+        setInterval(()=>{
+            this.setdisplayDateTime();
+            
+        }, 1000);
+    }
+
+    initButtonsEvnt(){
+        let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+    }
+
+    setdisplayDateTime(){
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale);
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+    }
+    get displayTime(){
+        return this._timeEl.innerHTML;
+    }
+
+    set displayTime(value){
+        return this._timeEl.innerHTML = value;
+    }
+
+    get displayDate(){
+        return this._dateEl.innerHTML;
+    }
+
+    set displayDate(value){
+        return this._dateEl.innerHTML = value;
     }
 
     get displayCalc(){
 
-        return this._displayCalc;
+        return this._displayCalcEl.innerHTML;
     }
 
-    set displayCalc(valor){
-        this._displayCalc = valor;
+    set displayCalc(value){
+        this._displayCalcEl.innerHTML = value;
     }
 
-    get dataAtual(){
-        return this._dataAtual;
+    get currentDate(){
+        return new Date();
     }
 
-    set dataAtual(valor){
-        this._dataAtual = valor;
+    set currentDate(value){
+        this._currentDate = value;
     }
 }
